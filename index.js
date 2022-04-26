354a5589c0dce60221199a58e184b89ebfef6cfe
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-27 00:44:55 +0600

    intially product data get from db

--- index.js
+++ index.js
@@ -17,15 +17,20 @@ const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
-
 async function run () {
     try{
         await client.connect(); //client.connect 
         const productCollection = client.db("Ema-John").collection("product");   //perform actions on the collection object
 
-    }
+        //inital data get
+        app.get('/product', async (req,res) =>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
 
+    }
     finally{
 
     }
