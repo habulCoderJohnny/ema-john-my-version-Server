9f1316d0e96bc70011ba8a97b916531cd68f9067
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-27 12:21:41 +0600

    Load data based on the page number and size - pagination & query

--- index.js
+++ index.js
@@ -22,20 +22,30 @@ async function run () {
         await client.connect(); //client.connect 
         const productCollection = client.db("Ema-John").collection("product");   //perform actions on the collection object
 
-        //inital data get
+        //inital data get/count/page/query
         app.get('/product', async (req,res) =>{
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
             const query = {};
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            //const products = await cursor.limit(10).toArray();
+            let products;
+            if (page || size) {
+                /*0 --> skip: 0 get: 0-10(10): 
+                 1 --> skip: 1*10 get: 11-20(10):
+                 2 --> skip: 2*10 get: 21-30 (10):
+                 3 --> skip: 3*10 get: 21-30 (10):*/
+               products = await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+                products = await cursor.toArray();  
+            }
             res.send(products);
         });
 
         //count Data (see total number of data) FOR Pagination
         app.get('/productCount', async(req,res)=>{
-            const query = {};
-            const cursor = productCollection.find(query);
-            const count = await cursor.count();
+            const count = await productCollection.estimatedDocumentCount();
             res.send({count}); //sent to as a object
         });
 
